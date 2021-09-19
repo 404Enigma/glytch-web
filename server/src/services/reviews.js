f65453bcd8aa6ffaac42cf4b57b1bb92ebcf0124
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2021-09-19 23:42:33 +0530

    dashboard updated

--- server/src/services/reviews.js
+++ server/src/services/reviews.js
@@ -1,5 +1,6 @@
 const admin = require("../config/db");
 const db = admin.firestore();
+const fbadmin = require("firebase-admin");
 
 const getReviews = async () => {
   const reviewRef = db.collection("Reviews");
@@ -29,4 +30,60 @@ const getFeedbacks = async () => {
   return feedback_data;
 };
 
-module.exports = { getReviews, add_feedback, getFeedbacks };
+const updateAnalytics = async (sentiment_value) => {
+  const Analytics_Ref = db.collection("Analytics");
+
+  const counts = await Analytics_Ref.doc("Company")
+    .get()
+    .then((doc) => {
+      total_count = doc.data().total_count;
+      sentiment_0 = doc.data().sentiment_0;
+      sentiment_1 = doc.data().sentiment_1;
+      sentiment_2 = doc.data().sentiment_2;
+      sentiment_3 = doc.data().sentiment_3;
+      return { total_count, sentiment_0, sentiment_1, sentiment_2, sentiment_3 };
+    });
+
+  counts.total_count = fbadmin.firestore.FieldValue.increment(1);
+
+  // let sentiment_category = {};
+  // let sentiment_0, sentiment_1, sentiment_2, sentiment_3;
+
+  console.log("sentiment_value: " + sentiment_value);
+
+  if (sentiment_value == 0) {
+    counts.sentiment_0 = fbadmin.firestore.FieldValue.increment(1);
+  } else if (sentiment_value == 1) {
+    counts.sentiment_1 = fbadmin.firestore.FieldValue.increment(1);
+  } else if (sentiment_value == 2) {
+    counts.sentiment_2 = fbadmin.firestore.FieldValue.increment(1);
+  } else if (sentiment_value == 3) {
+    counts.sentiment_3 = fbadmin.firestore.FieldValue.increment(1);
+  } else if (sentiment_value == 4) {
+    counts.sentiment_4 = fbadmin.firestore.FieldValue.increment(1);
+  }
+
+  //console.log("sentiment_category: " + sentiment_category.sentiment_0);
+  console.log("countss:", counts);
+
+  try {
+    await Analytics_Ref.doc("Company").update(counts);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const get_Analytics = async () => {
+  const Analytics_Ref = db.collection("Analytics");
+
+  const data_Ref = Analytics_Ref.doc("Company");
+  const doc = await data_Ref.get();
+  if (!doc.exists) {
+    console.log("No such document!");
+  } else {
+    console.log("Document data:", doc.data());
+    return doc.data();
+  }
+};
+
+module.exports = { getReviews, add_feedback, getFeedbacks, updateAnalytics, get_Analytics };
