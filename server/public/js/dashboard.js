02cc22f15d0646fd8460047ba6b0b1b01a402288
Pratyush Jain <pratyush.jain2001@gmail.com>
2021-09-19 21:45:33 +0530

    User Profile done

--- server/public/js/dashboard.js
+++ server/public/js/dashboard.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
     });
 })();
 
-$(window).load(function () {
-    //Do the code in the {}s when the window has loaded
-    $("#loader").fadeOut("fast"); //Fade out the #loader div
-});
+// $(window).load(function () {
+//     //Do the code in the {}s when the window has loaded
+//     $("#loader").fadeOut("fast"); //Fade out the #loader div
+// });
